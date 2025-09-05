269d528cd211486266fc323c89f9314e1e37d5ca
soohoioa <soohoioa@gmail.com>
2025-09-05 23:10:43 +0900

    어노테이션 추가

--- src/main/java/com/project/board0905/domain/category/repository/CategoryRepository.java
+++ src/main/java/com/project/board0905/domain/category/repository/CategoryRepository.java
@@ -2,9 +2,11 @@ package com.project.board0905.domain.category.repository;
 
 import com.project.board0905.domain.category.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
     boolean existsByName(String name);
     boolean existsBySlug(String slug);
--- src/main/java/com/project/board0905/domain/user/repository/UserRepository.java
+++ src/main/java/com/project/board0905/domain/user/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.project.board0905.domain.user.repository;
 
 import com.project.board0905.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
