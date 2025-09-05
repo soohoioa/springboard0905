e68830b6570f99f7f3f98a333a3022ef9bd98b76
soohoioa <soohoioa@gmail.com>
2025-09-05 14:21:40 +0900

    first commit

---
+++ src/main/java/com/project/board0905/Board0905Application.java
@@ -0,0 +1,13 @@
+package com.project.board0905;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board0905Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Board0905Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/board0905/Board0905ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.board0905;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board0905ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
